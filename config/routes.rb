51a5adabdc27b354ebca233f6acaf5d391e5a8d8
Konstantinos Loupos <louposk@gmail.com>
2012-05-27 23:56:11 +0300

    Finished RSpec test for contact controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 RailsPrservices::Application.routes.draw do
+
   get "static_pages/home"
-
   get "static_pages/help"
-
   get "static_pages/about"
+  get "static_pages/contact"
 
-  get "root/index"
+  # get "root/index"
 
   resources :customers
 
