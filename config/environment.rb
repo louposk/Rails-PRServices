2133e60d81b502b69de92b9beb8b246897b2c158
Konstantinos Loupos <louposk@gmail.com>
2012-06-01 16:56:12 +0300

    Env fix

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 RailsPrservices::Application.initialize!
 
-<<<<<<< HEAD
-# ActionMailer settings
-  ActionMailer::Base.smtp_settings = {
-  :address  => "mail.projectteam.gr",
-  :port  => 25,
-  :user_name  => "webmaster@projeccteam.gr",
-  :password  => "$*z1$*",
-  :authentication  => :login
-=======
 ActionMailer::Base.smtp_settings = {
 :user_name => "app4846269@heroku.com",
 :password => "happy61!",
@@ -21,5 +12,4 @@ ActionMailer::Base.smtp_settings = {
 :port => 587,
 :authentication => :plain,
 :enable_starttls_auto => true
->>>>>>> customer-mailer
 }
