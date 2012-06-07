fd1f051248098ec401aeaee06a0bfedeb16550cd
Konstantinos Loupos <louposk@gmail.com>
2012-06-07 23:29:52 +0300

    Added some styles

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -65,7 +65,7 @@ class CustomersController < ApplicationController
 
     respond_to do |format|
       if @customer.update_attributes(params[:customer])
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
+        format.html { redirect_to :back, notice: 'Customer was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
