65d1a3b090d7a9047f541ba997cff93148c7b50c
Konstantinos Loupos <louposk@gmail.com>
2012-06-07 21:39:55 +0300

    Commented hostingmailer

--- app/mailers/hosting_mailer.rb
+++ app/mailers/hosting_mailer.rb
@@ -1,9 +1,9 @@
 class HostingMailer < ActionMailer::Base
-    default from: "webmaster@projectteam.gr"
+#     default from: "webmaster@projectteam.gr"
 
-  def hosting_expires(hosting)
-    @hosting = hosting
-    @url  = "http://www.projectteam.gr"
-    mail(:to => @hosting.customer.email, :subject => "Project Team - Η υπηρεσίας σας λήγει")
-  end
-end
+#   def hosting_expires(hosting)
+#     @hosting = hosting
+#     @url  = "http://www.projectteam.gr"
+#     mail(:to => @hosting.customer.email, :subject => "Project Team - Η υπηρεσίας σας λήγει")
+#   end
+# end
